14184e693ac0de9262a35b9875f5eca1d5104ba2
spenser <spensertsai@gmail.com>
2026-05-17 16:33:02 +0800

    modified pi_string.py

--- chapter_10/pi_string.py
+++ chapter_10/pi_string.py
@@ -1,11 +1,13 @@
-filename = 'pi_million_digits.txt'
+from pathlib import Path
+
+filename = Path(__file__).parent / "pi_million_digits.txt"
 
 with open(filename) as file_object:
     lines = file_object.readlines()
 
-pi_string = ''
+pi_string = ""
 for line in lines:
     pi_string += line.strip()
-    
+
 print(f"{pi_string[:52]}...")
 print(len(pi_string))
