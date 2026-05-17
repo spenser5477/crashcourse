79e561b92c25d57b74045f614a5ea35132f4a0e6
spenser <spensertsai@gmail.com>
2026-05-17 09:24:06 +0800

    Modified chapter9 my-cars.py

--- chapter_09/importing_classes/my_cars.py
+++ chapter_09/importing_classes/my_cars.py
@@ -1,7 +1,9 @@
-import car
+# This is a sample code to demonstrate importing classes from another file.
+# import car
+from car import Car, ElectricCar
 
-my_beetle = car.Car('volkswagen', 'beetle', 2019)
+my_beetle = Car("volkswagen", "beetle", 2019)
 print(my_beetle.get_descriptive_name())
 
-my_tesla = car.ElectricCar('tesla', 'roadster', 2019)
+my_tesla = ElectricCar("tesla", "roadster", 2019)
 print(my_tesla.get_descriptive_name())
