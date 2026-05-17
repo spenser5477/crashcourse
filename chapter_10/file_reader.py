c1085a2563a25806d9fa4147d07e86cb53d97897
spenser <spensertsai@gmail.com>
2026-05-17 11:49:31 +0800

    Chapter 10 file_reader.py 用pathlib中Path轉成絕對路徑

--- chapter_10/file_reader.py
+++ chapter_10/file_reader.py
@@ -1,7 +1,17 @@
-filename = 'pi_digits.txt'
+from pathlib import Path
 
-with open(filename) as file_object:
-    lines = file_object.readlines()
+filename = Path(__file__).parent / "pi_digits.txt"
+
+print(filename)
+
+try:
+    with filename.open(encoding="utf-8") as file_object:
+        lines = file_object.readlines()
+except FileNotFoundError:
+    print(f"File not found: {filename}")
+    raise
 
 for line in lines:
-    print(line.rstrip())
+    print(
+        line.rstrip()
+    )  # rstrip() removes the trailing newline character from each line before printing it.
