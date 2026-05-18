896840a3d7c389bff0a7a78c545840cad8b63955
spenser <spensertsai@gmail.com>
2026-05-18 23:46:05 +0800

    Modified write_message.py

--- chapter_10/write_message.py
+++ chapter_10/write_message.py
@@ -1,5 +1,9 @@
-filename = 'programming.txt'
+from pathlib import Path
 
-with open(filename, 'a') as file_object:
+# filename = 'programming.txt'    #檔案被寫在上一層的目錄中
+
+filename = Path(__file__).parent / "programming.txt"
+
+with open(filename, "a") as file_object:
     file_object.write("I also love finding meaning in large datasets.\n")
     file_object.write("I love creating apps that can run in a browser.\n")
