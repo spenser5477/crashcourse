8eb19f97e6ac980221cd43d845810f964fd128c1
spenser <spensertsai01@gmail.com>
2026-05-25 13:58:01 +0800

    15.2 plot line

--- chapter_15/plotting_simple_line_graph/mpl_squares.py
+++ chapter_15/plotting_simple_line_graph/mpl_squares.py
@@ -3,14 +3,22 @@ import matplotlib.pyplot as plt
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
-plt.style.use("default")
+# 系統中可使用的樣式  >>> plt.style.available
+
+# plt.style.use("seaborn-v0_8")
+plt.style.use('seaborn-v0_8-colorblind')
+# 設定支援繁體中文的字體
+plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']  # Windows 微軟正黑體
+# 或 macOS 使用：['Heiti TC'] 或 ['Arial Unicode MS']
+plt.rcParams['axes.unicode_minus'] = False  # 解決負號顯示為方塊
+
 fig, ax = plt.subplots()
 ax.plot(input_values, squares, linewidth=3)
 
 # Set chart title and label axes.
-ax.set_title("Square Numbers", fontsize=24)
-ax.set_xlabel("Value", fontsize=14)
-ax.set_ylabel("Square of Value", fontsize=14)
+ax.set_title("平方數", fontsize=24)
+ax.set_xlabel("值", fontsize=14)
+ax.set_ylabel("值的平方", fontsize=14)
 
 # Set size of tick labels.
 ax.tick_params(axis="both", labelsize=14)
--- chapter_15/plotting_simple_line_graph/scatter_squares.py
+++ chapter_15/plotting_simple_line_graph/scatter_squares.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 x_values = range(1, 1001)
 y_values = [x**2 for x in x_values]
 
-plt.style.use('seaborn')
+plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
@@ -19,3 +19,4 @@ ax.tick_params(axis='both', which='major', labelsize=14)
 ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
+#plt.savefig("scatter_squares_1.png", bbox_inches='tight')  
