d7f2a74baeea3835ba0b422e4dc8c3c7fc9606a9
spenser <spensertsai01@gmail.com>
2026-05-27 11:27:31 +0800

    17.1 Modified python_repos.py

--- chapter_17/python_repos.py
+++ chapter_17/python_repos.py
@@ -8,11 +8,17 @@ print(f"Status code: {r.status_code}")
 
 # Store API response in a variable.
 response_dict = r.json()
+print(response_dict.keys())
 print(f"Total repositories: {response_dict['total_count']}")
 
 # Explore information about the repositories.
 repo_dicts = response_dict['items']
 print(f"Repositories returned: {len(repo_dicts)}")
+
+repo_dict = repo_dicts[0]
+# print(f"\nKeys: {len(repo_dict)}")
+# for key in sorted(repo_dict.keys()):
+#   print(key)
 
 print("\nSelected information about each repository:")
 for repo_dict in repo_dicts:
@@ -23,3 +29,4 @@ for repo_dict in repo_dicts:
     print(f"Created: {repo_dict['created_at']}")
     print(f"Updated: {repo_dict['updated_at']}")
     print(f"Description: {repo_dict['description']}")
+    print("---------------------------------------------")
